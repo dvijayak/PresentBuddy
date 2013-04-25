d01bac098cfc90e8169bbe693eeb111fb1e28f1d
Daniel <dvijayak@uoguelph.ca>
2013-04-25 01:48:02 -0400

    Begin refactoring of code for better readability.

---
+++ src/Util.cpp
@@ -0,0 +1,25 @@
+/*
+ * Util.cpp
+ *
+ *  Created on: 2013-04-25
+ *      Author: pilgrim
+ */
+
+#include "Util.h"
+
+namespace javelind {
+namespace bb {
+namespace pbuddy {
+
+Util::Util() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Util::~Util() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace pbuddy */
+} /* namespace bb */
+} /* namespace javelind */
---
+++ src/Util.h
@@ -0,0 +1,24 @@
+/*
+ * Util.h
+ *
+ *  Created on: 2013-04-25
+ *      Author: pilgrim
+ */
+
+#ifndef UTIL_H_
+#define UTIL_H_
+
+namespace javelind {
+namespace bb {
+namespace pbuddy {
+
+class Util {
+public:
+	Util();
+	virtual ~Util();
+};
+
+} /* namespace pbuddy */
+} /* namespace bb */
+} /* namespace javelind */
+#endif /* UTIL_H_ */
